0691f8d8f8854cdb436bb65e10e99ef416c8874a
Evan Martin <evan.martin@gmail.com>
2025-12-17 11:24:30 -0800

    accept default tsserver formatting

--- states.tsx
+++ states.tsx
@@ -1,8 +1,14 @@
-import * as preact from 'preact';
-import { Signal, signal, useComputed, useSignal, useSignalEffect } from "@preact/signals";
+import * as preact from "preact";
+import {
+  Signal,
+  signal,
+  useComputed,
+  useSignal,
+  useSignalEffect,
+} from "@preact/signals";
 
 interface Loc {
-  region: string,
+  region: string;
   name: string;
   gdp: number;
   gdpPerCapita: number;
@@ -10,28 +16,30 @@ interface Loc {
   pop: number;
 }
 
-type LocField = 'gdp' | 'land' | 'pop';
-type LandUnit = 'mi' | 'km';
-const landUnit = signal<LandUnit>('mi');
+type LocField = "gdp" | "land" | "pop";
+type LandUnit = "mi" | "km";
+const landUnit = signal<LandUnit>("mi");
 
-const gdpUnit = signal<'' | 'per capita'>('');
+const gdpUnit = signal<"" | "per capita">("");
 
-function parse(region: 'us' | 'eu', tsv: string): Loc[] {
-  return tsv.split('\n').map(line => {
-    const [name, gdpStr, landStr, popStr] = line.split('\t');
+function parse(region: "us" | "eu", tsv: string): Loc[] {
+  return tsv.split("\n").map((line) => {
+    const [name, gdpStr, landStr, popStr] = line.split("\t");
     let gdp = parseFloat(gdpStr);
     let land = parseFloat(landStr);
     const pop = parseFloat(popStr);
-    if (region === 'eu') {
-      gdp *= 1000;  // normalize to $m
-      land /= 2.59;  // normalize to sqmi
+    if (region === "eu") {
+      gdp *= 1000; // normalize to $m
+      land /= 2.59; // normalize to sqmi
     }
     const gdpPerCapita = gdp / pop;
     return { region, name, gdp, gdpPerCapita, land, pop };
   });
 }
 
-const us = parse('us', `California	3598103	163694.74	39029342
+const us = parse(
+  "us",
+  `California	3598103	163694.74	39029342
 Texas	2355960	268596.46	30029572
 New York	2053180	54554.98	19677151
 Florida	1389070	65757.70	22244823
@@ -80,9 +88,12 @@ South Dakota	67571	77115.68	909824
 Montana	65015	147039.71	1122867
 Alaska	63618	665384.04	733583
 Wyoming	47433	97813.01	581381
-Vermont	40617	9616.36	647064`);
+Vermont	40617	9616.36	647064`,
+);
 
-const eu = parse('eu', `Germany	4308.850	357386	83408554
+const eu = parse(
+  "eu",
+  `Germany	4308.850	357386	83408554
 United Kingdom	3158.938	242495	67281039
 France	2923.489	551695	64531444
 Italy	2169.745	301318	59240329
@@ -130,9 +141,10 @@ Montenegro	7.027	13812	627859
 Andorra	3.669	468	79034
 San Marino	1.807	61	33745
 Liechtenstein	6.872	160	39039
-Monaco	6.817	2	36686`);
+Monaco	6.817	2	36686`,
+);
 
-function match(src: Loc, field: LocField | 'gdpPerCapita', dsts: Loc[]) {
+function match(src: Loc, field: LocField | "gdpPerCapita", dsts: Loc[]) {
   const dists = dsts.map((dst, i) => {
     const dist = dst[field] - src[field];
     return { i, dist };
@@ -142,91 +154,144 @@ function match(src: Loc, field: LocField | 'gdpPerCapita', dsts: Loc[]) {
 }
 
 function relPct(a: number, b: number) {
-  const pct = ((b - a) * 100 / a);
+  const pct = ((b - a) * 100) / a;
   let str = `${pct.toFixed(1)}%`;
   if (pct > 0) {
-    str = '+' + str;
+    str = "+" + str;
   }
   return <small>{str}</small>;
 }
 
 function showStat(loc: Loc, field: LocField): string {
   switch (field) {
-    case 'gdp': {
-      if (gdpUnit.value === 'per capita') {
+    case "gdp": {
+      if (gdpUnit.value === "per capita") {
         return `${(loc.gdpPerCapita * 1000).toFixed(1)}k`;
       } else {
         return `${(loc.gdp / 1000).toFixed(1)}b`;
       }
     }
-    case 'land': {
+    case "land": {
       let land = loc.land;
-      if (landUnit.value === 'km') land *= 2.59;
+      if (landUnit.value === "km") land *= 2.59;
       land /= 1000;
       return `${land.toFixed(0)}k`;
     }
-    case 'pop': return `${(loc.pop / 1_000_000).toFixed(1)}`;
-  };
+    case "pop":
+      return `${(loc.pop / 1_000_000).toFixed(1)}`;
+  }
 }
 
 /** Table with verbose stats about the single selected location. */
 function Selected({ loc }: { loc: Loc }) {
-  return <div>
-    <table>
-      <tr><td class='r'>GDP:</td><td>${showStat(loc, 'gdp')} USD&nbsp;
-        <select value={gdpUnit} onChange={e => gdpUnit.value = ((e.target as HTMLOptionElement).value as ('' | 'per capita'))}>
-          <option value=''></option>
-          <option value='per capita'>per capita</option>
-        </select>
-      </td></tr>
-      <tr><td class='r'>Land:</td><td>{showStat(loc, 'land')}&nbsp;
-        <select value={landUnit} onChange={e => landUnit.value = ((e.target as HTMLOptionElement).value as LandUnit)}>
-          <option value='mi'>sq mi</option>
-          <option value='km'>sq km</option>
-        </select>
-      </td></tr>
-      <tr><td class='r'>Population:</td><td>{showStat(loc, 'pop')} million</td></tr>
-    </table>
-  </div>;
+  return (
+    <div>
+      <table>
+        <tr>
+          <td class="r">GDP:</td>
+          <td>
+            ${showStat(loc, "gdp")} USD&nbsp;
+            <select
+              value={gdpUnit}
+              onChange={(e) =>
+                (gdpUnit.value = (e.target as HTMLOptionElement).value as
+                  | ""
+                  | "per capita")
+              }
+            >
+              <option value=""></option>
+              <option value="per capita">per capita</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td class="r">Land:</td>
+          <td>
+            {showStat(loc, "land")}&nbsp;
+            <select
+              value={landUnit}
+              onChange={(e) =>
+                (landUnit.value = (e.target as HTMLOptionElement)
+                  .value as LandUnit)
+              }
+            >
+              <option value="mi">sq mi</option>
+              <option value="km">sq km</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td class="r">Population:</td>
+          <td>{showStat(loc, "pop")} million</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
-function Combobox({ text, options }: { text: Signal<string>, options: string[] }) {
+function Combobox({
+  text,
+  options,
+}: {
+  text: Signal<string>;
+  options: string[];
+}) {
   const focus = useSignal(false);
 
   let filteredOptions = useComputed(() => {
-    const filtered = options.filter(o => o !== text.value && o.toLowerCase().startsWith(text.value.toLowerCase()));
+    const filtered = options.filter(
+      (o) =>
+        o !== text.value &&
+        o.toLowerCase().startsWith(text.value.toLowerCase()),
+    );
     return filtered.slice(0, 5);
   });
   let optionsDOM;
   if (focus.value) {
     if (filteredOptions.value.length > 0) {
-      optionsDOM = <div class='popup'>
-        {filteredOptions.value.map(o => <div class='entry'
-          onMouseDown={(e) => e.preventDefault()}  // prevent blur event
-          onClick={() => text.value = o}>{o}</div>)}
-      </div>;
+      optionsDOM = (
+        <div class="popup">
+          {filteredOptions.value.map((o) => (
+            <div
+              class="entry"
+              onMouseDown={(e) => e.preventDefault()} // prevent blur event
+              onClick={() => (text.value = o)}
+            >
+              {o}
+            </div>
+          ))}
+        </div>
+      );
     }
   }
 
-  return <div class='combo'>
-    <input type='text' value={text}
-      onFocus={() => focus.value = true}
-      onBlur={() => focus.value = false}
-      onInput={e => text.value = ((e.target as HTMLInputElement).value)}
-      onKeyDown={e => {
-        if (e.key === 'Enter' && filteredOptions.value.length > 0) {
-          text.value = filteredOptions.value[0];
-        }
-      }}
-      onChange={e => text.value = (e.target as HTMLInputElement).value} />
-    {optionsDOM}
-  </div>;
+  return (
+    <div class="combo">
+      <input
+        type="text"
+        value={text}
+        onFocus={() => (focus.value = true)}
+        onBlur={() => (focus.value = false)}
+        onInput={(e) => (text.value = (e.target as HTMLInputElement).value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && filteredOptions.value.length > 0) {
+            text.value = filteredOptions.value[0];
+          }
+        }}
+        onChange={(e) => (text.value = (e.target as HTMLInputElement).value)}
+      />
+      {optionsDOM}
+    </div>
+  );
 }
 
-const allRegions = us.concat(eu).map(loc => loc.name).sort();
+const allRegions = us
+  .concat(eu)
+  .map((loc) => loc.name)
+  .sort();
 
 function Picker({ loc }: { loc: Signal<Loc | undefined> }) {
-  const locName = useSignal('');
+  const locName = useSignal("");
   useSignalEffect(() => {
     let cur = us.find((loc) => loc.name === locName.value);
     if (!cur) {
@@ -235,65 +300,93 @@ function Picker({ loc }: { loc: Signal<Loc | undefined> }) {
     loc.value = cur;
   });
 
-  return <h2>
-    Region: <Combobox text={locName} options={allRegions} />
-  </h2>;
+  return (
+    <h2>
+      Region: <Combobox text={locName} options={allRegions} />
+    </h2>
+  );
 }
 
-function Comparables({ src, top, axis }: { src: Loc, top: Loc[], axis: Signal<LocField> }) {
+function Comparables({
+  src,
+  top,
+  axis,
+}: {
+  src: Loc;
+  top: Loc[];
+  axis: Signal<LocField>;
+}) {
   function row(loc: Loc) {
-    return <tr>
-      <td>{loc.name}</td>
-      <td class='r'>{showStat(loc, 'gdp')}</td>
-      <td class='r'>{gdpUnit.value === '' ? relPct(src.gdp, loc.gdp) : relPct(src.gdpPerCapita, loc.gdpPerCapita)}</td>
-      <td class='r'>{showStat(loc, 'land')}</td>
-      <td class='r'>{relPct(src.land, loc.land)}</td>
-      <td class='r'>{showStat(loc, 'pop')}</td>
-      <td class='r'>{relPct(src.pop, loc.pop)}</td>
-    </tr>;
+    return (
+      <tr>
+        <td>{loc.name}</td>
+        <td class="r">{showStat(loc, "gdp")}</td>
+        <td class="r">
+          {gdpUnit.value === ""
+            ? relPct(src.gdp, loc.gdp)
+            : relPct(src.gdpPerCapita, loc.gdpPerCapita)}
+        </td>
+        <td class="r">{showStat(loc, "land")}</td>
+        <td class="r">{relPct(src.land, loc.land)}</td>
+        <td class="r">{showStat(loc, "pop")}</td>
+        <td class="r">{relPct(src.pop, loc.pop)}</td>
+      </tr>
+    );
   }
 
-  return <div>
-    <h2>Comparables, by{' '}
-      <select value={axis} onChange={e => axis.value = ((e.target as HTMLSelectElement).value as LocField)}>
-        <option value='gdp'>GDP</option>
-        <option value='land'>Land</option>
-        <option value='pop'>Population</option>
-      </select>
-    </h2>
-    <table width='100%'>
-      <tr>
-        <th></th>
-        <th colSpan={2}>GDP (${gdpUnit.value === '' ? '' : '/person'})</th>
-        <th colSpan={2}>Land (sq {landUnit})</th>
-        <th colSpan={2}>Pop (m)</th>
-      </tr>
-      {top.map(row)}
-    </table>
-  </div>;
+  return (
+    <div>
+      <h2>
+        Comparables, by{" "}
+        <select
+          value={axis}
+          onChange={(e) =>
+            (axis.value = (e.target as HTMLSelectElement).value as LocField)
+          }
+        >
+          <option value="gdp">GDP</option>
+          <option value="land">Land</option>
+          <option value="pop">Population</option>
+        </select>
+      </h2>
+      <table width="100%">
+        <tr>
+          <th></th>
+          <th colSpan={2}>GDP (${gdpUnit.value === "" ? "" : "/person"})</th>
+          <th colSpan={2}>Land (sq {landUnit})</th>
+          <th colSpan={2}>Pop (m)</th>
+        </tr>
+        {top.map(row)}
+      </table>
+    </div>
+  );
 }
 
-function Location({ loc, axis }: { loc: Signal<Loc>, axis: Signal<LocField> }) {
+function Location({ loc, axis }: { loc: Signal<Loc>; axis: Signal<LocField> }) {
   const top = useComputed(() => {
-    let field: LocField | 'gdpPerCapita' = axis.value;
-    if (field === 'gdp' && gdpUnit.value === 'per capita') {
-      field = 'gdpPerCapita';
+    let field: LocField | "gdpPerCapita" = axis.value;
+    if (field === "gdp" && gdpUnit.value === "per capita") {
+      field = "gdpPerCapita";
     }
-    return match(loc.value, field, loc.value.region === 'us' ? eu : us);
+    return match(loc.value, field, loc.value.region === "us" ? eu : us);
   });
-  return <div>
-    <Selected loc={loc.value!} />
-    <Comparables src={loc.value!} top={top.value} axis={axis} />
-  </div>;
+  return (
+    <div>
+      <Selected loc={loc.value!} />
+      <Comparables src={loc.value!} top={top.value} axis={axis} />
+    </div>
+  );
 }
 
 function UI() {
   const loc = useSignal<Loc | undefined>(undefined);
-  const axis = useSignal<LocField>('gdp');
-  return <div>
-    <Picker loc={loc} />
-    {loc.value ? <Location loc={loc as Signal<Loc>} axis={axis} /> : null}
-  </div>;
+  const axis = useSignal<LocField>("gdp");
+  return (
+    <div>
+      <Picker loc={loc} />
+      {loc.value ? <Location loc={loc as Signal<Loc>} axis={axis} /> : null}
+    </div>
+  );
 }
 
-preact.render(<UI />, document.getElementById('out')!);
+preact.render(<UI />, document.getElementById("out")!);
